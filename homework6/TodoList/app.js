46208f271854ed776a8f0623377e9b7f8f227cea
Jbhardwaj21 <jasleen.bhardwaj@gmail.com>
2016-02-06 15:53:43 -0800

    added api work

--- homework6/TodoList/app.js
+++ homework6/TodoList/app.js
@@ -1,3 +1,40 @@
+// *click
+// - listen for the click of the button
+// - (button.addEventListener("click", add stuff to the list))
+
+//
+// *append it the parent (adding stuff to the list)
+// identify the parent (ul)
+// attach child (li)
+// create more children (li)
+// we place input.value inside newly created child element
+// now, we append this new child+input.value  onto parent element
+//
 
 
+// grabbing things...
+// ========
+// grab the form element === >
+// grab the input element ==== >
+// grab the ul element === >
 
+var form = document.querySelector("#new-todo-list-form");
+var input = document.querySelector("#new-todo-item-input");
+var parent = document.querySelector("#todo-list")
+
+function addListItem(todo) {
+  var listItem = document.createElement("li");
+  li.textContent = todo;
+
+  parent.appendChild("listItem");
+}
+
+function formSubmitted(buttonClicked){
+  buttonClicked.preventDefault();
+  addListItem(input.value);
+  form.reset();
+}
+
+// formsubitted is a "call back function" which does not require a double paranthesis
+// it does not run immediately.
+form.addEventListener("submit",formSubmitted);
